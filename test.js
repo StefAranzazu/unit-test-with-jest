be5a0d3e147069f2c22b0661c867acac052184bb
StefAranzazu <stefanyaranzazu1310@gmail.com>
2021-07-13 18:54:27 +0000

    Unit testing with Jest

---
+++ test.js
@@ -0,0 +1,28 @@
+// import the function sum from the app.js file
+const { sum } = require('./app.js');
+
+// start your first test
+test('adds 14 + 9 to equal 23', () => {
+    //inside the test we call our sum function with 2 numbers
+    let total = sum(14, 9);
+
+    // we expect the sum of those 2 numbers to be 23
+    expect(total).toBe(23);
+});
+test("One euro should be 1.206 dollars", function(){
+    // importo la funcion desde app.js
+    const { fromEuroToDollar } = require('./app.js')
+    
+    // hago mi comparacion (la prueba)
+    expect(fromEuroToDollar(3.5)).toBe(4.2); //1 euro son 1.2 dolares, entonces 3.5 euros deberian ser = (3.5 * 1.2)
+})
+test("1.206 dollars should be 154.247 Yen", function(){
+    const { fromDollarToYen } = require('./app.js')
+    
+    expect(fromDollarToYen (3.5)).toBe(447.65000000000003); //1 euro son 1.2 dolares, entonces 3.5 euros deberian ser = (3.5 * 1.2)
+})
+test("154.247 Yen should be 123.397 pound", function(){
+    const { fromYenToPound } = require('./app.js')
+    
+    expect(fromYenToPound (3.5)).toBe(2.8000000000000003); //1 euro son 1.2 dolares, entonces 3.5 euros deberian ser = (3.5 * 1.2)
+})
